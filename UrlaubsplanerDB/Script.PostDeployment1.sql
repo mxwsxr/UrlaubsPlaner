440f45397a04fc8a267c2d332d88591998e68fc8
Torben Clasen <torbenclasen@gmail.com>
2019-07-20 17:32:53 +0200

    Added Germany to DB

---
+++ UrlaubsplanerDB/Script.PostDeployment1.sql
@@ -0,0 +1,17 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+If EXISTS (Select * FROM [dbo].Country WHERE Name='Deutschland')
+BEGIN
+	INSERT INTO [dbo].Country (CountryID,Name,Code)
+	VALUES (NEWID(),'Deutschland','DE')
+END
