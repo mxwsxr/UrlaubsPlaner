8448da924613607f28a9634ff186c6d07589b41f
Torben Clasen <torbenclasen@gmail.com>
2019-07-20 22:19:09 +0200

    Creating Basic Querys

---
+++ UrlaubsPlaner/DBInteraction/AbsenceTypeQuerys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlaubsPlaner.DBInteraction
+{
+    public static class AbsenceTypeQuerys
+    {
+        public static readonly string GETABSENCETYPES = @"SELECT [AbsenceTypeID]
+      ,[Label]
+  FROM [dbo].[AbsenceType]";
+    }
+}
---
+++ UrlaubsPlaner/DBInteraction/EmployeeQuerys.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlaubsPlaner.DBInteraction
+{
+    public static class EmployeeQuerys
+    {
+        public static readonly string GETEMPLOYEES = @"SELECT [EmployeeID]
+      ,[EmployeeNumber]
+      ,[CountryID]
+      ,[Firstname]
+      ,[Lastname]
+      ,[Birthday]
+      ,[Street]
+      ,[Housenumber]
+      ,[Postalcode]
+      ,[City]
+      ,[Phonenumber]
+      ,[Email]
+  FROM [dbo].[Employee]";
+    }
+}
