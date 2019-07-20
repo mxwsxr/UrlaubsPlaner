07ba643ce2488309c9eee04d861a0c3eaa7610ba
Torben Clasen <torbenclasen@gmail.com>
2019-07-20 22:27:57 +0200

    Added View to display all relevant info for absences

---
+++ UrlaubsplanerDB/Views/AbsencesWithEmployeeAndAbsenceType.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[AbsencesWithEmployeeAndAbsenceType]
+	AS 
+	SELECT a.[AbscenceID]
+	  ,a.[FromDate]
+      ,a.[ToDate]
+      ,a.[Reason]
+	  ,e.[EmployeeNumber]
+	  ,e.[Firstname]
+      ,e.[Lastname]
+	  ,t.[Label]
+	FROM [dbo].[Absence] as a 
+	join [dbo].[Employee] as e on a.[EmployeeID] = e.[EmployeeID]
+	join [dbo].[AbsenceType] as t on a.[AbsenceTypeID] = t.[AbsenceTypeID]
