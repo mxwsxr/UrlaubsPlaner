100d2335150843165617f0c3107b84aa364f71e9
Torben Clasen <torbenclasen@gmail.com>
2019-08-15 21:04:25 +0200

    Added the SqlCommand Parameter setups

--- UrlaubsPlaner/DBInteraction/UpsertCommandSetups.cs
+++ UrlaubsPlaner/DBInteraction/UpsertCommandSetups.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UrlaubsPlaner.Entities;
 
 namespace UrlaubsPlaner.DBInteraction
 {
-    internal static class UpsertCommandSetups
+    public static class UpsertCommandSetups
     {
+        public static SqlCommand SetupAbsenceTypeCommand(SqlCommand sqlCommand, AbsenceType absenceType)
+        {
+            sqlCommand.Parameters.AddWithValue(nameof(absenceType.AbsenceTypeId), absenceType.AbsenceTypeId);
+            sqlCommand.Parameters.AddWithValue(nameof(absenceType.Label), absenceType.Label);
+            return sqlCommand;
+        }
+
+        public static SqlCommand SetupAbsenceCommand(SqlCommand sqlCommand, Absence absence)
+        {
+            sqlCommand.Parameters.AddWithValue(nameof(absence.AbsenceID), absence.AbsenceID);
+            sqlCommand.Parameters.AddWithValue(nameof(absence.AbsenceType), absence.AbsenceType.AbsenceTypeId);
+            sqlCommand.Parameters.AddWithValue(nameof(absence.Employee), absence.Employee.EmployeeId);
+            sqlCommand.Parameters.AddWithValue(nameof(absence.FromDate), absence.FromDate);
+            sqlCommand.Parameters.AddWithValue(nameof(absence.Reason), absence.Reason);
+            return sqlCommand;
+        }
+
+        public static SqlCommand SetupEmployeeCommand(SqlCommand sqlCommand, Employee employee)
+        {
+            sqlCommand.Parameters.AddWithValue(nameof(employee.EmployeeId), employee.EmployeeId);
+            sqlCommand.Parameters.AddWithValue(nameof(employee.Country), employee.Country.CountryId);
+            sqlCommand.Parameters.AddWithValue(nameof(employee.Firstname), employee.Firstname);
+            sqlCommand.Parameters.AddWithValue(nameof(employee.Lastname), employee.Lastname);
+            sqlCommand.Parameters.AddWithValue(nameof(employee.Birthday), employee.Birthday);
+            sqlCommand.Parameters.AddWithValue(nameof(employee.Street), employee.Street);
+            sqlCommand.Parameters.AddWithValue(nameof(employee.Housenumber), employee.Housenumber);
+            sqlCommand.Parameters.AddWithValue(nameof(employee.Postalcode), employee.Postalcode);
+            sqlCommand.Parameters.AddWithValue(nameof(employee.City), employee.City);
+            sqlCommand.Parameters.AddWithValue(nameof(employee.Phonenumber), employee.Phonenumber);
+            sqlCommand.Parameters.AddWithValue(nameof(employee.Email), employee.Email);
+            sqlCommand.Parameters.AddWithValue(nameof(employee.EmployeeNumber), employee.EmployeeNumber);
+            return sqlCommand;
+        }
     }
 }
